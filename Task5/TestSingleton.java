a16730753eafa2baaa6c1a4801cc54dd0d68b238
Aibol Tungatarov <aibolseed@gmail.com>
2018-10-18 09:58:38 +0600

    added Testing with Threads in Task5 Abstraction

--- Task5/TestSingleton.java
+++ Task5/TestSingleton.java
@@ -2,15 +2,29 @@ package Task5;
 
 public class TestSingleton {
     public static void main(String args[]){
-        LoggerSingleton logger1 = LoggerSingleton.getInstance();
-        LoggerSingleton logger2 = LoggerSingleton.getInstance();
-        LoggerSingleton logger3 = LoggerSingleton.getInstance();
-        logger1.logError("11111", "HIGH");
-        logger1.logConnection("100.10.0.82", "18:12");
-        logger1.logChange("Folder bin", "00:42");
-        logger2.logError("11251", "LOW");
-        logger2.logConnection("122.10.0.7", "11:36");
-        logger3.logConnection("28.100.0.19", "06:37");
-        logger3.logChange("File exam.txt", "07:44");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LoggerSingleton logger1 = LoggerSingleton.getInstance();
+                logger1.logError("113211", "HIGH");
+                logger1.logConnection("122.13.1.3", "12:36");
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LoggerSingleton logger2 = LoggerSingleton.getInstance();
+                logger2.logError("11251", "LOW");
+                logger2.logConnection("122.10.0.7", "11:36");
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LoggerSingleton logger3 = LoggerSingleton.getInstance();
+                logger3.logConnection("28.100.0.19", "06:37");
+                logger3.logChange("File exam.txt", "07:44");
+            }
+        }).start();
     }
 }
